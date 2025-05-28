34730d97d275c3d0a0f89c320ee68a6c533d8591
SamuelBrudner <samuel.brudner@gmail.com>
2025-05-28 17:48:16 -0400

    test: ensure Code path added in analyze_crimaldi test

--- tests/test_analyze_crimaldi_data.py
+++ tests/test_analyze_crimaldi_data.py
@@ -1,7 +1,11 @@
 import os
+import sys
 import unittest
 
 import pytest
+
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 np = pytest.importorskip("numpy")
 h5py = pytest.importorskip("h5py")
